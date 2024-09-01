f4098171d9f4593a50456483e03f33b4419d11cf
alfred <lianghongbuaa@gmail.com>
2024-09-01 12:08:01 +0800

    add rolling array solution

--- problems/dp/climbStairs.ts
+++ problems/dp/climbStairs.ts
@@ -16,8 +16,22 @@ function dp(n: number): number {
     return result;
 }
 
+// rolling array
+function climbStairs2(n: number): number {
+    let p = 0, q = 0, r = 1;
+    // f(0) = 1, f(1) = 1, f(2) = 2
+    // f(n) = f(n-1) + f(n-2)
+    for (let i = 1; i <= n; i++) {
+        p = q;
+        q = r;
+        r = p + q;
+    }
+    return r;
+}
+
 function climbStairs(n: number): number {
-    return dp(n);
+    return climbStairs2(n);
+    // return dp(n);
 }
 
 export default climbStairs;
